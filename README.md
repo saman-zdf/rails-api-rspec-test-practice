f10c15b191525574a077514a59936358a37ccbf9
saman-zdf <samanfyz68@gmail.com>
2021-11-18 14:37:26 +0800

    readme file has been updated

--- README.md
+++ README.md
@@ -522,3 +522,49 @@ at the top of your RSpec.describe in the posts_spec.rb.
 ---
 
 - Adding user controller with jwt
+  First we need to generate controller for the user model, we will name the controller auth. when it's generated, we need to write some tests in the request/auth_spec.rb:
+
+```Ruby
+#  Get /index need to be change to POST /auth/login
+  beofer(:all) do
+    @user = create(:user)
+  end
+
+  context "with correct credentials" do
+    # I can change the params data by passing new params data like below, it will change it with the existing data from factory
+    before(:each) do
+      post '/auth/login', params: {auth: {email: "sam@test.com", password: "password1"}}
+    end
+
+    it "should respond with 200 ok" do
+      expect(response).to have_http_status(200)
+    end
+    it "should respond with correct content type" do
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+    end
+
+    it "should respond with correct username and jwt" do
+      # whatever you have as a username in your factory or you can overwrite that by passing as above
+      expect(response.body).to include("test")
+      expect(response.body).to include("jwt")
+    end
+  end
+
+  context "with incorrect credentials" do
+    # I can change the params data by passing new params data like below, it will change it with the existing data from factory
+    before(:each) do
+      post '/auth/login', params: {auth: {email: "test@test.com", password: "incorrectPass"}}
+    end
+
+    it "should respond with 422 ok" do
+      expect(response).to have_http_status(422)
+    end
+    it "should respond with correct content type" do
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+    end
+
+    it "should respond with error message" do
+      expect(response.body).to include("Incorrect credentials!!!")
+    end
+  end
+```
--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -7,42 +7,41 @@ RSpec.describe "Auths", type: :request do
       @user = create(:user)
     end
     # context for the correct credentials
-    context 'with correct credentials' do
-
+    context "with correct credentials" do 
+      # I can change the params data by passing new params data like below, it will change it with the existing data from factory
       before(:each) do
-        post '/auth/login', params: {auth: {email: "sam@test.com", password: "password1" }}
+        post '/auth/login', params: {auth: {email: "sam@test.com", password: "password1"}}
       end
 
-      it 'should return 200 ok' do
+      it "should respond with 200 ok" do
         expect(response).to have_http_status(200)
       end
-
-      it "should response a correct content type" do
+      it "should respond with correct content type" do
         expect(response.content_type).to eq("application/json; charset=utf-8")
       end
-      it "should response body a correct username and jwt" do
+
+      it "should respond with correct username and jwt" do
+        # whatever you have as a username in your factory or you can overwrite that by passing as above
         expect(response.body).to include("Saman")
         expect(response.body).to include("jwt")
       end
-      
     end
-    # end of context for the correct credentials
-    # context for testing the incorrect credentials
-    context 'with incorrect credentials' do
+    # context for incorrect credentials
+    context "with incorrect credentials" do 
+      # I can change the params data by passing new params data like below, it will change it with the existing data from factory
       before(:each) do
-        post '/auth/login', params: {auth: {email: "sam@test.com", password: "password1212" }}
+        post '/auth/login', params: {auth:{email: "sam@test.com", password: "incorrectPass"}}
       end
 
-      it 'should return 404 ok' do
+      it "should respond with 422 ok" do
         expect(response).to have_http_status(422)
       end
-
-      it "should response a correct content type" do
+      it "should respond with correct content type" do
         expect(response.content_type).to eq("application/json; charset=utf-8")
       end
-      it "should response body with error message" do
+
+      it "should respond with error message" do
         expect(response.body).to include("Incorrect credentials!!!")
-        
       end
     end
     # end of context for testing the incorrect credentials
